127be7f4ce81739415e6b76e1c5ddaf9f0d2446f
fahlevialir <mukhammad.fahlevi@insignia.co.id>
2025-11-12 15:07:10 +0700

    fixed format bmiCalculator tools in tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -6,6 +6,13 @@ type bmiCalculator struct{
 	height float64
 }
 
+func (s bmiCalculator) Name() string(
+	return "bmi_calculator"
+)
+
+func (s bmiCalculator) Description() string{
+	return "Calculates BMI (Body Mass Index) based on weight (kg) and height (m)."
+}
 func BmiCalculator(weight float64, height float64) float64 {
 	calculatedBmi:= weight/(math.Pow(height,2))
 	return calculatedBmi
